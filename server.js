2a46ef2ea8d1454e149f1ecb58071a67f148adc6
Pureshwar <pureshwargonekar7@gmail.com>
2023-09-09 01:15:52 +0530

    updated

--- server.js
+++ server.js
@@ -7,21 +7,41 @@ const port = process.env.PORT || 3001;
 
 app.use(cors());
 
-// Serve the static React build files
-app.use(express.static('weather-app/build'));
+// Route to fetch latitude and longitude using the geo API
+app.get('/getCoords', async (req, res) => {
+  const city = req.query.city; // Get the city from the query parameter
 
-app.get('/weather', async (req, res) => {
   try {
-    const response = await axios.get(
-      'https://api.openweathermap.org/data/2.5/weather?lat=22.0578244&lon=78.9386203&appid=4287408eae10a96af0b40f382a259c1a'
-    );
-    res.json(response.data);
+    const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=4287408eae10a96af0b40f382a259c1a`);
+    const data = response.data[0]; // Assuming you want the first result
+
+    if (!data) {
+      throw new Error('Location not found');
+    }
+
+    const { lat, lon } = data;
+    res.json({ lat, lon });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while fetching location data.' });
+  }
+});
+
+// Route to fetch weather data using latitude and longitude
+app.get('/getWeather', async (req, res) => {
+  const lat = req.query.lat; // Get latitude from the query parameter
+  const lon = req.query.lon; // Get longitude from the query parameter
+
+  try {
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4287408eae10a96af0b40f382a259c1a`);
+    const weatherData = response.data;
+    
+    res.json(weatherData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred while fetching weather data.' });
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- src/App.js
+++ src/App.js
@@ -1,41 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import axios from 'axios'; // Import Axios
+import axios from 'axios';
+import CircularProgress from '@mui/material/CircularProgress'; // Import CircularProgress from Material-UI
 
 function App() {
+  const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false); // State to track loading status
 
-  useEffect(() => {
-    axios.get('http://localhost:3001/weather') // Use Axios to make the GET request
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true); // Start loading when the form is submitted
+
+    axios
+      .get(`http://localhost:3001/getCoords?city=${city}`)
       .then((response) => {
         if (response.status !== 200) {
           throw new Error('Network response was not ok');
         }
-        return response.data;
+        const { lat, lon } = response.data;
+        return axios.get(`http://localhost:3001/getWeather?lat=${lat}&lon=${lon}`);
       })
-      .then((data) => {
-        console.log("data", data, "typeof", typeof data);
-        setWeatherData(data);
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error('Network response was not ok');
+        }
+        setWeatherData(response.data);
       })
       .catch((error) => {
         console.error(error);
+      })
+      .finally(() => {
+        setLoading(false); // Stop loading after the API request is complete
       });
-  }, []);
+  };
+  const bgcolor = {
+    background: 'linear-gradient(103deg, rgba(223,221,255,1) 15%, rgba(255,255,250,1) 92%, rgba(255,255,250,1) 95%)'
+  };
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        {weatherData ? (
-          <div>
-            <h2>{weatherData.name}</h2>
+    <div className="App ">
+      <header className="App-header ">
+        <h1 className='font-serif text-4xl mb-10'>Weather App</h1>
+        <form className='flex justify-center' onSubmit={handleFormSubmit}>
+          <input
+            className='text-black h-12  rounded-sm pl-2'
+            type="text"
+            placeholder="Enter city name"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            required
+          />
+          <button className='h-12 ml-1 rounded-sm text-base bg-lime-500 hover:bg-lime-600 p-2 mb-5' type="submit">Get Weather</button>
+        </form>
+        {loading ? ( // Display loader while loading is true
+          <CircularProgress color="secondary" />
+        ) : weatherData ? (
+          <div style={bgcolor} className='border-4 p-7 rounded-xl border-lime-500 font-normal bg-white text-blue-900 shadow-[0_35px_60px_-15px_rgba(10,30,0.3)] hover:shadow-lg'>
+            <h2 className='font-semibold'>{weatherData.name}, {weatherData.sys.country}</h2>
             <p>{weatherData.weather[0].description}</p>
-            <p>Temperature: {weatherData.main.temp}K ({(weatherData.main.temp- 273.15).toFixed(2)}°C) </p>
-            <p>Humidity: {weatherData.main.humidity}% </p>
-            <p>Wind Speed: {weatherData.wind.speed}</p>
+            <p>Temperature: {weatherData.main.temp}K ({(weatherData.main.temp - 273.15).toFixed(2)}°C)</p>
+            <p>Humidity: {weatherData.main.humidity}%</p>
+            <p>Wind Speed: {weatherData.wind.speed} m/s</p>
           </div>
         ) : (
-          <p>Loading...</p>
+          <p>Enter a city name and click "Get Weather" <br /> to fetch weather information.</p>
         )}
       </header>
     </div>
